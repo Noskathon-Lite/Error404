c11e261ba93ee7858623f6b013341c7d34be51ed
Aayush Kunwar <aayushkunwar03@gmail.com>
2025-01-12 13:28:41 +0545

    feat: handle basic folder architecture and routing

---
+++ nativereact/ai-trip-planner/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2>Welcome to Error 404</h2>
+     <button>Login</button>
+    </>
+  )
+}
+
+export default App
